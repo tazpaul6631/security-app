9de0ac40d0fc63b016acb4bca06ae999cb83e8a0
JIAHSIN\Thuan.Truong <tazpaul6631@gmail.com>
2026-03-06 07:32:16 +0700

    Fix: update github actions syntax and sign-app version

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -15,11 +15,11 @@ const config: CapacitorConfig = {
       logging: false, // Tắt log nội bộ của SQLite để console sạch hơn
     },
   },
-  // server: {
-  //   // 10.0.2.2 là địa chỉ IP máy tính host khi nhìn từ Emulator
-  //   url: 'http://10.0.149.28:8100',
-  //   cleartext: true // Cho phép chạy HTTP (không cần HTTPS)
-  // }
+  server: {
+    // 10.0.2.2 là địa chỉ IP máy tính host khi nhìn từ Emulator
+    url: 'http://10.0.149.28:8100',
+    cleartext: true // Cho phép chạy HTTP (không cần HTTPS)
+  }
 };
 
 export default config;
